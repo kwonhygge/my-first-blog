49ad86212da3c1a39dc1fa24ba12dd246f65166b
dory <godrl0106@naver.com>
2019-09-07 16:24:29 +0900

    savebirth done

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class SaveBirth(models.Model):
+    name = models.CharField(max_length=30)
+    birth_day = models.DateField(blank=True, null=True)
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.http import JsonResponse
-from .models import Post
+from .models import Post, SaveBirth
 from .forms import PostForm
 from django.shortcuts import redirect
 from django.views.decorators.csrf import csrf_exempt
+import datetime
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -43,6 +45,18 @@ def post_edit(request, pk):
 
 @csrf_exempt
 def save_birth(request):
+    user_name = request.POST.get("user_name", None)
+    birthday = request.POST.get("text", None)
+
+    birth_day = datetime.datetime.strptime(birthday, "%Y/%M/%d").date()
+
+    SaveBirth.objects.create(
+        name=user_name,
+        birth_day=birth_day,
+    )
+
     return JsonResponse({
-        "text": "weather is good",
+        'text': 'created',
+        'message' : user_name,
+        'birthday' : birth_day,
     })
